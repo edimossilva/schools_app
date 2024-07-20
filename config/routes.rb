129ad6e248f056ee9ccd24122366fe7d3391330b
Edimo Silva <edimossilva@gmail.com>
2024-07-20 08:27:36 -0300

    Initialize 129-move-everything-related-to-hello-world-or-example-to-packs

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "sidekiq/web"
+
+Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  post "/graphql", to: "graphql#execute"
+  mount Sidekiq::Web => "/sidekiq"
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+  devise_for :users, controllers: { omniauth_callbacks: "oauth/omniauth_callbacks" }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get "/public_method", to: "hello_world#public_method"
+  get "/private_method", to: "hello_world#private_method"
+  get "/search", to: "hello_world#search"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get "/public_method", to: "hello_world#public_method"
+      get "/private_method", to: "hello_world#private_method"
+
+      resources :users, only: [:index]
+    end
+  end
+end
