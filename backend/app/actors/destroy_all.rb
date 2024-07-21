785633e38f1b673d764a05cb580c7112281a3769
Edimo <edimo@spectora.com>
2024-07-21 10:40:57 -0300

    Create SyncSchools actor

--- backend/app/actors/destroy_all.rb
+++ backend/app/actors/destroy_all.rb
@@ -6,16 +6,20 @@ class DestroyAll < Actor
   def call
     self.data = [objects_summary]
 
-    School.with_deleted.destroy_all
-    SchoolSearch.with_deleted.destroy_all
-    SearchSchoolParam.with_deleted.destroy_all
-    School.with_deleted.destroy_all
-    SchoolSearch.with_deleted.destroy_all
-    SearchSchoolParam.with_deleted.destroy_all
+    destroy_all
+    destroy_all
 
     Rails.cache.clear
 
     data.push(objects_summary)
+  end
+
+  private
+
+  def destroy_all
+    School.with_deleted.destroy_all
+    SchoolSearch.with_deleted.destroy_all
+    SearchSchoolParam.with_deleted.destroy_all
   end
 
   def objects_summary
