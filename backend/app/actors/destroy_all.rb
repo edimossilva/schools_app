97b230075baeb777f63fe6a93eb7ec0abeeaa4db
Edimo <edimo@spectora.com>
2024-07-21 10:21:51 -0300

    Add service to sync school data

--- backend/app/actors/destroy_all.rb
+++ backend/app/actors/destroy_all.rb
@@ -4,12 +4,22 @@ class DestroyAll < Actor
   output :data
 
   def call
-    SearchSchoolParam.with_deleted.destroy_all
-    SchoolSearch.with_deleted.destroy_all
+    self.data = [objects_summary]
+
     School.with_deleted.destroy_all
+    SchoolSearch.with_deleted.destroy_all
+    SearchSchoolParam.with_deleted.destroy_all
+    School.with_deleted.destroy_all
+    SchoolSearch.with_deleted.destroy_all
+    SearchSchoolParam.with_deleted.destroy_all
+
     Rails.cache.clear
 
-    self.data = {
+    data.push(objects_summary)
+  end
+
+  def objects_summary
+    {
       SearchSchoolParam: SearchSchoolParam.with_deleted.count,
       SchoolSearch: SchoolSearch.with_deleted.count,
       School: School.with_deleted.count
